09ecdb7870ddc419b2e493e11abf2ec3cbdab31a
lucas-sales <lucasg17.lima@gmail.com>
2020-05-13 10:31:50 -0300

    code pattern

--- bot_dice.py
+++ bot_dice.py
@@ -1,27 +1,31 @@
 import discord
 from discord.ext import commands
 from random import randint
-client = commands.Bot(command_prefix = "/" )
+
+PREFIX              = "/"
+BOT_MESSAGE_INIT    = "bot ready"
+BOT_MESSAGE_ERROR   = "Wrong Command"
+ROLL_COMMAND        = "r"
+
+client = commands.Bot(command_prefix = PREFIX )
 
 @client.event
 async def on_ready():
-    print("bot ready!")
+    print(BOT_MESSAGE_INIT)
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error,commands.MissingRequiredArgument):
-        await ctx.send("wrong command!")
+        await ctx.send(BOT_MESSAGE_ERROR)
 
-
-@client.command(name = "r")
+@client.command(name = ROLL_COMMAND)
 async def roll(ctx, exp, mod = 0):
-
         number, dice, mod = [exp[0], exp[2:], mod]    
         number  = int(number)
         dice    = int(dice)
-        await ctx.send(d20(dice, number, mod))
+        await ctx.send(roll(dice, number, mod))
 
-def d20(dice, number, mod=0):
+def roll(dice, number, mod=0):
     roll = 0
     for i in range(1, number+1):
         roll += randint(1,dice)
